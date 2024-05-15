fd11d7d86185df28c1660a0de35572c60c44618d
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-05-15 19:22:24 +0600

    removing node_modules

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173',],
+  origin: ['http://localhost:5173',
+  'helps-hand-network-server.vercel.app',
+  'https://helps-hand-network.web.app'],
   credentials: true
 }));
 app.use(express.json());
